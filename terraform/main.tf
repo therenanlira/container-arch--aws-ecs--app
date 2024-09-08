5725622c6d47577092e601c7307e695135b001fc
Renan Lira <renanlira@bluems.com>
2024-09-08 20:55:53 -0300

    Add Terraform configuration for AWS ECS IAM role and policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,14 @@
 module "service" {
   source = "../../container-arch--aws-ecs-module/"
 
-  cluster_name   = var.cluster_name
-  service_name   = var.service_name
-  service_port   = var.service_port
-  service_cpu    = var.service_cpu
-  service_memory = var.service_memory
+  cluster_name         = var.cluster_name
+  service_name         = var.service_name
+  service_port         = var.service_port
+  service_cpu          = var.service_cpu
+  service_memory       = var.service_memory
+  service_listener_arn = data.aws_ssm_parameter.listener_arn.value
+
+  service_task_execution_role_arn = aws_iam_role.service_task_execution_role.arn
 
   vpc_id = data.aws_ssm_parameter.vpc_id.value
 
@@ -15,5 +18,4 @@ module "service" {
     data.aws_ssm_parameter.private_subnet_1c.value
   ]
 
-  service_listener_arn = data.aws_ssm_parameter.listener_arn.value
 }
